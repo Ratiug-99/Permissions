870716921357f89bbac619e6dbc4c2cd3850ec77
Evgeniy Basiuk <evgeniy.basiuk@gmail.com>
2020-10-02 07:49:38 +0300

    delete DownloadLoader.java Create DownloadManager

--- app/src/main/java/com/ratiug/dev/permissions/MainActivity.java
+++ app/src/main/java/com/ratiug/dev/permissions/MainActivity.java
@@ -1,33 +1,32 @@
 package com.ratiug.dev.permissions;
 
 import android.Manifest;
+import android.app.DownloadManager;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.loader.app.LoaderManager;
-import androidx.loader.content.Loader;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Bitmap> {
+public class MainActivity extends AppCompatActivity {
     private static final String TAG = "DBG | MainActivity";
     private static final int CODE_PERMISSION_WRITE_EXTERNAL = 1;
-    private static final int CODE_PERMISSION_INTERNET= 2;
     Button btn1, btn2;
     EditText etText1, etText2;
-    ImageView image;
-    Loader mLoader;
+    ImageView imageView;
     String path;
+    Bitmap picture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,15 +35,13 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
         initializeViews();
         checkWriteExternalPermission();
-        checkInternetPermission();
-
 
     }
 
     private void initializeViews() {
         btn1 = findViewById(R.id.btn_one);
         btn2 = findViewById(R.id.btn_two);
-        image = findViewById(R.id.iv_iamge);
+        imageView = findViewById(R.id.iv_iamge);
         etText1 = findViewById(R.id.et_text);
         etText2 = findViewById(R.id.et_text2);
         btn2.setEnabled(false);
@@ -73,48 +70,21 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, CODE_PERMISSION_WRITE_EXTERNAL);
     }
 
-    private void checkInternetPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
-                Log.d(TAG, "onCreate: Permission internet granted!");
-            } else {
-                Log.d(TAG, "onCreate: Permission internet denied!");
-                requestPermissionExternalWrite();
-            }
-        }
-    }
-
-    private void requestPermissionInternet() {
-        Log.d(TAG, "RequestPermissionInternet");
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, CODE_PERMISSION_INTERNET);
-    }
-
-
-
 
     private void downloadImage() {
         Log.d(TAG, "downloadImage");
         path = String.valueOf(etText1.getText());
-        getSupportLoaderManager().restartLoader(1, new Bundle(), this).forceLoad();
-        mLoader = getSupportLoaderManager().getLoader(1);
+        //    Log.d(TAG, "downloadImage: " + path.substring()); //todo check format (jpg/jpeg/png)
         //  getBitmapFromURL(src);
-    }
+        String url = path;
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+        request.setDescription("Downloading...");
+        request.setTitle("Picture");
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "name-of-the-file.jpg");
 
-    @NonNull
-    @Override
-    public Loader<Bitmap> onCreateLoader(int id, @Nullable Bundle args) {
-        Log.d(TAG, "onCreateLoader");
-        return new DownloadLoader(this, path);
-    }
-
-    @Override
-    public void onLoadFinished(@NonNull Loader<Bitmap> loader, Bitmap data) {
-        Log.d(TAG, "onLoadFinished " + data.toString());
-        image.setImageBitmap(data);
-    }
-
-    @Override
-    public void onLoaderReset(@NonNull Loader<Bitmap> loader) {
-        Log.d(TAG, "onLoaderReset");
+// get download service and enqueue file
+        DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+        manager.enqueue(request);
     }
 }
