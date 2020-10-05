e073ab5f1a1ac099312c28d8bf09a3af44082183
Evgeniy Basiuk <evgeniy.basiuk@gmail.com>
2020-10-05 05:03:05 +0300

    Create Broadcast Receiver Finish Download

--- app/src/main/java/com/ratiug/dev/permissions/MainActivity.java
+++ app/src/main/java/com/ratiug/dev/permissions/MainActivity.java
@@ -2,9 +2,13 @@ package com.ratiug.dev.permissions;
 
 import android.Manifest;
 import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -20,6 +24,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "DBG | MainActivity";
     private static final int CODE_PERMISSION_WRITE_EXTERNAL = 1;
@@ -27,7 +33,11 @@ public class MainActivity extends AppCompatActivity {
     EditText etText1, etText2;
     ImageView imageView;
     String path;
+    String lastDownloadPath;
     Bitmap picture;
+    BroadcastReceiver broadcastReceiverFinishDownload;
+    ///
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +47,14 @@ public class MainActivity extends AppCompatActivity {
         initializeViews();
         checkWriteExternalPermission();
 
+        broadcastReceiverFinishDownload = new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                btn2.setEnabled(true);
+            }
+        };
+        IntentFilter filterFinishDownload = new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
+        registerReceiver(broadcastReceiverFinishDownload,filterFinishDownload);
     }
 
 
@@ -78,15 +96,27 @@ public class MainActivity extends AppCompatActivity {
         path = String.valueOf(etText1.getText());
         if (path.endsWith(".jpg") || path.endsWith(".jpeg") || path.endsWith(".png") || path.endsWith(".bmp")) {
             String url = path;
+            String  filename = path.substring(path.lastIndexOf("/") + 1);
+            Log.d(TAG, "downloadImage: " + filename);
             DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
             request.setDescription("Downloading...");
-            request.setTitle("Picture");
+            request.setTitle(filename);
             request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "name-of-the-file.jpg");
+            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, filename);
 
 // get download service and enqueue file
             DownloadManager manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
             manager.enqueue(request);
+            lastDownloadPath =  "Internal shared storage/Download/" + "android-1.jpg"; //+ filename
+            Log.d(TAG, "downloadImage: " + lastDownloadPath); //todo create correct path to last download file
+            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), filename);
+            if(file.exists()){
+                Log.d(TAG, "downloadImage: " + filename.toString());
+                picture = BitmapFactory.decodeFile(String.valueOf(file));
+                imageView.setImageBitmap(picture);
+            }
+
+
         } else {
             Toast.makeText(this, "Incorrect link", Toast.LENGTH_SHORT).show();
         }
